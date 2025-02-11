6eb79655190d77772c706f83f268054930949c17
IZY BOY <tanyimathieu@gmail.com>
2025-02-11 11:15:40 +0100

    update

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,18 +1,21 @@
-import java.util.Objects;
 import java.util.Scanner;
 
 public class Main {
-    public static void main(String[] args) throws Exception {
+    public static void main(String[] args) {
          System.out.print("$ ");
 
         Scanner scanner = new Scanner(System.in);
         String input = scanner.nextLine();
 //        System.out.println(input + ": command not found");
-        do {
+        /*do {
             System.out.println(input + ": command not found");
             System.out.print("$ ");
             input = scanner.nextLine();
-        } while (!input.matches(""));
+        } while (!input.matches(""));*/
+
+        while (System.out.println(input + ": command not found")) {
+            System.out.print("$ exit 0");
+        }
         scanner.close();
     }
 
