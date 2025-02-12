283ff4c9265ced1126db0df4a0517898734628db
IZY BOY <tanyimathieu@gmail.com>
2025-02-12 04:31:18 +0100

    Update Main.java
    exit

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -3,17 +3,32 @@ import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) throws Exception {
-         System.out.print("$ ");
+        System.out.print("$ ");
 
         Scanner scanner = new Scanner(System.in);
         String input = scanner.nextLine();
-//        System.out.println(input + ": command not found");
-        do {
+
+        //The invalid command loop
+        /*while (!input.matches("")) {
             System.out.println(input + ": command not found");
-            System.out.print("$ ");
+            System.out.print("$ exit ");
             input = scanner.nextLine();
-        } while (!input.matches(""));
-        scanner.close();
+        }
+        scanner.close();*/
+
+        //The exit command
+        while (!input.matches("")) {
+
+            if(input.equals("0")) {
+                scanner.close();
+            }
+            else {
+                System.out.println(input + ": command not found");
+                System.out.print("$ exit ");
+                input = scanner.nextLine();
+            }
+
+        }
     }
 
 }
